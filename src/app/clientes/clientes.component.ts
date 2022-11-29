45c9030a48c74209892bd80c4e13255666f5073f
Sef99 <enriquecaballero01@hotmail.com>
2022-11-29 18:48:51 -0300

    add localStorage y update a Clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
+import { LocalService } from '../local.service';
 import { ConfirmationDialog } from '../shared/confirm-delete.component';
 import { Cliente } from './clientes.model';
+
+
+@Component({
+  selector: 'select-dialog',
+  templateUrl: './open-dialog.html',
+})
+export class DialogOverviewExampleDialogCliente {
+  formGroup!: FormGroup;
+  cliente!: Cliente;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogCliente>,
+    @Inject(MAT_DIALOG_DATA) public data: Cliente, private formBuilder: FormBuilder, private localStorage: LocalService
+  ) {}
+
+  ngOnInit(){
+    this.formGroup = this.formBuilder.group({
+      'nombreApellido': [null, Validators.required],
+      'email': [null, Validators.required]
+    });
+    this.formGroup.get('nombreApellido')?.setValue(this.data.nombreApellido);
+    this.formGroup.get('email')?.setValue(this.data.email);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit() {
+    const ruc = this.data.ruc
+    const nombreApellido = this.formGroup.value.nombreApellido
+    const email = this.formGroup.value.email
+    const newCliente = new Cliente(ruc, nombreApellido, email);
+    console.log(newCliente)
+    this.localStorage.updateCliente(newCliente);
+  }
+}
 
 @Component({
   selector: 'app-clientes',
@@ -11,19 +50,17 @@ import { Cliente } from './clientes.model';
   styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent {
-  displayedColumns: string[] = ['ruc', 'nombreApellido', 'email', 'eliminar'];
+  displayedColumns: string[] = ['ruc', 'nombreApellido', 'email', 'eliminar', 'update'];
   dataSource = new MatTableDataSource<Cliente>();
-  data: Cliente[] = [
-    { ruc: 5479354, nombreApellido: 'Foneca', email: 'foneca@pol.edu.py' },
-    { ruc: 4608081, nombreApellido: 'Junior', email: 'junior@pol.edu.py' },
-    { ruc: 1212121, nombreApellido: 'Junior Blanco', email: 'junior2@pol.edu.py' },
-  ];
+  data: Cliente[] = [];
   dataObject = Object.assign(this.data);
 
 
-  constructor(private dialog: MatDialog, private snackBar: MatSnackBar) { };
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar, private localStorage: LocalService) { };
 
   ngOnInit() {
+    this.data = this.localStorage.getClientes();
+    this.dataObject = Object.assign(this.data)
     this.dataSource = new MatTableDataSource<Cliente>(this.dataObject)
   }
 
@@ -40,9 +77,8 @@ export class ClientesComponent {
 
     dialogRef.afterClosed().subscribe((confirmed: boolean) => {
       if (confirmed) {
-        this.data.splice(this.data.indexOf(cliente), 1);
-        console.log(this.dataSource.data)
-
+        
+        this.localStorage.deleteCliente(cliente)
         this.dataSource = new MatTableDataSource<Cliente>(this.dataObject)
 
         const a = document.createElement('a');
@@ -55,9 +91,26 @@ export class ClientesComponent {
     });
   }
 
-  addElement(producto: Cliente) {
-    this.data.push(producto);
+  addElement(cliente: Cliente) {
+    let result = this.localStorage.addCliente(cliente)
+    if (result == null) {
+      alert('El cliente ya existe');
+    }
     this.dataSource = new MatTableDataSource<Cliente>(this.dataObject)
   }
+
+  openUpdate(cliente: Cliente): void {
+    
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogCliente, {
+      width: '250px',
+      data: cliente
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      
+    });
+  }
+
 
 }
