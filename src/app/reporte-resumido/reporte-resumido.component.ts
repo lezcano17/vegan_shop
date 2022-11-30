3a5a8ba1cb7883efddda33591283ae7dba97e586
lezcano17 <jlezcano17@outlook.com>
2022-11-29 22:52:28 -0300

    add filtro cliente

--- src/app/reporte-resumido/reporte-resumido.component.ts
+++ src/app/reporte-resumido/reporte-resumido.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
+import { Cliente } from '../clientes/clientes.model';
 import { LocalService } from '../local.service';
 import { RegistroVentasProducto } from '../registro-ventas-productos/registro-ventas-productos.model';
 
@@ -14,11 +15,14 @@ export class ReporteResumidoComponent {
   dataSource = new MatTableDataSource<RegistroVentasProducto>();
   data: RegistroVentasProducto[] = []
   dataObject = Object.assign(this.data)
-  formGroupHeader!: FormGroup;
+  formGroupFecha!: FormGroup;
+  selectedClient!: Cliente;
 
   fechaDesde: any;
   fechaHasta: any;
   rucCliente: any;
+  clientes!: Cliente[];
+  formGroupCliente!: FormGroup;
 
   constructor(private localStorage: LocalService, private formBuilder: FormBuilder) { }
 
@@ -26,19 +30,23 @@ export class ReporteResumidoComponent {
     this.data = this.localStorage.getRegistros();
     this.dataObject = Object.assign(this.data);
     this.createForms();
+    this.clientes = this.localStorage.getClientes();
     this.dataSource = new MatTableDataSource<RegistroVentasProducto>(this.dataObject);
   }
 
   createForms() {
-    this.formGroupHeader = this.formBuilder.group({
+    this.formGroupFecha = this.formBuilder.group({
       'fechaDesde': [null, Validators.required],
       'fechaHasta': [null, Validators.required],
     });
+    this.formGroupCliente = this.formBuilder.group({
+      'cliente': [null, Validators.required]
+    });
   }
 
-  onSubmit(){
-    this.fechaDesde = this.formGroupHeader.value.fechaDesde;
-    this.fechaHasta = this.formGroupHeader.value.fechaHasta;
+  onSubmit() {
+    this.fechaDesde = this.formGroupFecha.value.fechaDesde;
+    this.fechaHasta = this.formGroupFecha.value.fechaHasta;
     this.fechaDesde = this.fechaDesde.getTime();
     this.fechaHasta = this.fechaHasta.getTime();
 
@@ -49,7 +57,7 @@ export class ReporteResumidoComponent {
     let elemento: any;
     let copia = [];
 
-    for(let i = 0; i < this.data.length; i++){
+    for (let i = 0; i < this.data.length; i++) {
       elemento = this.data[i]
       let date1 = new Date(elemento.fecha).getTime();
       let date2 = new Date(this.fechaDesde).getTime();
@@ -58,12 +66,28 @@ export class ReporteResumidoComponent {
 
       if (date1 >= date2 && date1 <= date3)
         copia.push(this.data[i]);
-      }
-
-      this.data = copia
-      console.log(copia)
-      this.dataObject = Object.assign(this.data);
-      this.dataSource = new MatTableDataSource<RegistroVentasProducto>(this.dataObject);
-
     }
+
+    this.data = copia
+    console.log(copia)
+    this.dataObject = Object.assign(this.data);
+    this.dataSource = new MatTableDataSource<RegistroVentasProducto>(this.dataObject);
+
   }
+  filtrarCliente() {
+    let elemento: any;
+    let copia: RegistroVentasProducto[] = [];
+
+    for (let i = 0; i < this.data.length; i++) {
+      elemento = this.data[i];
+      console.log(this.selectedClient.nombreApellido)
+      if (this.selectedClient.nombreApellido === elemento.cliente.nombreApellido) {
+        copia.push(this.data[i]);
+      }
+    }
+    this.data = copia
+    console.log(copia)
+    this.dataObject = Object.assign(this.data);
+    this.dataSource = new MatTableDataSource<RegistroVentasProducto>(this.dataObject);
+  }
+}
